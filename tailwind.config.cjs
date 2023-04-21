6a1bf4cdf9401717d28b8dd5e342cf53a4559224
“Mia <mb.mia.bosse@gmail.com>
2023-04-21 09:20:31 +0200

    sektioner tilføjet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      bg: "#FBFBFB",
+      footer: "#FF0000",
+    },
+
+    fontFamily: {
+      montserrat: "'montserrat', sans-serif;",
+      archivo: "'archivo', sans-serif;",
+      tangerine: "'tangerine', sans-serif;",
+    },
+
+    extend: {
+      screens: {
+        xxs: "374px",
+        xs: "540px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1423px",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
